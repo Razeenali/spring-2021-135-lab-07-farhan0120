3e098c5eb757f96b15146ae88b063b76cf498498
Razeenali <64715453+Razeenali@users.noreply.github.com>
2021-04-22 12:18:11 -0400

    Update unindent.cpp

--- unindent.cpp
+++ unindent.cpp
@@ -34,27 +34,6 @@ string removeLeadingSpaces(string line){
     return new_line;   
 }
 
-string findFirstchar(string line){
-    //The purpose of this function is to find a non space character which would be used to find a curly space so the 
-    //indentation can be fixed
-
-    string first_char;
-    int dummy = 0;
-    for (int i = 0; i < line.length(); i++)
-    {
-        if (isspace(line[i]))
-        {
-            dummy++;
-        }
-        else{
-            //Returns the non-space first character
-            first_char = line[i];
-            return first_char;
-        }
-    
-    }
-    return first_char;
-}
 
 int countChar(string line, char c){
     //This counts the number of specified character which would be used to find the curly braces
@@ -69,38 +48,35 @@ int countChar(string line, char c){
     
 }
 
+string getTab(int num){
+  string tab = "";
+  if(num == 0){
+    return tab;
+  }
+  for(int i = 0; i < num; i++){
+    tab += "\t";
+  }
+  return tab;
+}
+
 string indent_code(string filename){
   string reformatted = "";
   ifstream file(filename);
   int num_of_braces = 0;
   string first_char;
+  int mcount = 0;
   
   //Thus reads line by line we use getline so we dont get individual characters 
   for (string line; getline(file,line);){
-      first_char = findFirstchar(line);
-      //If the first non space character is a cloing brace we tab one less than usual
-      if (first_char == "}"){
-          for (int tab=0; tab < num_of_braces-1; tab++){
-             reformatted += "\t";
-             }
-        }
-      else{
-          //This tabs depending on the code blocks which are defined by curly braces
-          for (int tab=0; tab < num_of_braces; tab++){
-              reformatted += "\t"; 
-              } 
-      }
-      //Adds new line to the code
-      reformatted += removeLeadingSpaces(line) + "\n";
-      num_of_braces += countChar(line, '{');
-      num_of_braces -= countChar(line, '}');
-    }
+    mcount = mcount - countChar(removeLeadingSpaces(line),'}');
+    reformatted += getTab(mcount) + removeLeadingSpaces(line) + "\n";
+    mcount = mcount + countChar(removeLeadingSpaces(line),'{');
 
-    //file.close() wasnt working so this was my substitute
+  }
   if (reformatted == ""){
       reformatted = "File not found please try again";
   }
-
+  
   return reformatted;
 }
   
